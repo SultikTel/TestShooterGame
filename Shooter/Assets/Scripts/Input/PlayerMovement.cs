9102eedbf3abe5097cde9733c6d74e6fc6087cd6
SultikTel <sultiktel@gmail.com>
2026-04-09 23:09:34 +0500

    Base movement added

---
+++ Shooter/Assets/Scripts/Input/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using Shooter.Input;
+using UnityEngine;
+
+namespace Shooter
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _rotationSpeed;
+        private Transform _camera;
+        private Rigidbody _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+            _camera = Camera.main.transform;
+        }
+
+        private void Update()
+        {
+            HandleRotation();
+        }
+
+        private void FixedUpdate()
+        {
+            HandleMovement();
+        }
+
+        private void HandleMovement()
+        {
+            Vector2 moveInput = InputController.Instance.actions.Gameplay.Move.ReadValue<Vector2>();
+            Vector3 moveDirection = (_camera.forward * moveInput.y) + (_camera.right * moveInput.x);
+            moveDirection.Normalize();
+            _rb.velocity = moveDirection * _moveSpeed;
+        }
+
+        private void HandleRotation()
+        {
+            Vector2 moveInput = InputController.Instance.actions.Gameplay.Move.ReadValue<Vector2>();
+            Vector3 targetDirection = (_camera.forward * moveInput.y) + (_camera.right * moveInput.x);
+            targetDirection.Normalize();
+            if (targetDirection == Vector3.zero)
+            {
+                targetDirection = transform.forward;
+            }
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+            Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+            transform.rotation = playerRotation;
+        }
+    }
+}
